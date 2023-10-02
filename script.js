8e7abfa51dfc150aabc8f105aab66c7fb5989dda
JObengDappah <134343671+JObengDappah@users.noreply.github.com>
2023-10-02 15:19:51 -0400

    js script coded/modified

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
-// Assignment code here
 
+function generatePassword() {
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    let passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters):"));
+  
+ 
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+      alert("Please enter a valid password length between 8 and 128 characters.");
+      return "";
+    }
+  
+ 
+    let useLowercase = confirm("Include lowercase characters?");
+    let useUppercase = confirm("Include uppercase characters?");
+    let useNumeric = confirm("Include numeric characters?");
+    let useSpecial = confirm("Include special characters?");
+  
+    
+    if (!useLowercase && !useUppercase && !useNumeric && !useSpecial) {
+      alert("You must select at least one character type.");
+      return "";
+    }
+  
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numericChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+";
+  
+   
+    let characterPool = "";
+    if (useLowercase) characterPool += lowercaseChars;
+    if (useUppercase) characterPool += uppercaseChars;
+    if (useNumeric) characterPool += numericChars;
+    if (useSpecial) characterPool += specialChars;
+  
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * characterPool.length);
+      password += characterPool.charAt(randomIndex);
+    }
+  
+    return password;
+  }
+  
+  
+  function writePassword() {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+  
+    if (password) {
+      passwordText.value = password;
+    }
+  }
+  
+  
+  let generateBtn = document.querySelector("#generate");
+  
+  generateBtn.addEventListener("click", writePassword);
+  
